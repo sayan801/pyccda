144c8e90cf4a8d89391ecec7fc870863cde7e5c9
Abhisek Roy <abhisekroycse@gmail.com>
2016-12-02 20:21:36 +0530

    Add files via upload
    Hi, I have tried to parse given C-CDA Xml files to create ProptoRPC message and then JSON file for allergy module. I also uploaded modified pyccda/ccda.py for allergy and then I have done the required modification in ccda_to_json.py file. Run the code using following commands:
    python ccda_to_json.py testdata/CCDA_CCD_b1_Ambulatory_v2.xml

--- ccda_to_json.py
+++ ccda_to_json.py
@@ -1,22 +1,38 @@
 import pyccda,xmltodict,json
-
 import sys, string, os
 
 length = len(sys.argv)
-
 print "Parameters: %s"%length
-
-if length != 2 :
+if length != 2:
     raise Exception("Usage: Python Filename XmlFileName")
-
-XmlFileName = sys.argv[1]
-
+XmlFileName = str(sys.argv[1])
 print "XmlFileName: %s"%XmlFileName
-
 #section = sys.argv[2]
 
-ccda = pyccda.CcdaDocument(open(XmlFileName) )    #CCDA_CCD_b1_Ambulatory_v2.xml'))
+ccda = pyccda.CcdaDocument(open(XmlFileName))    #CCDA_CCD_b1_Ambulatory_v2.xml')) # Creation of ccda object for C-CDA XML file
 
-ccda_message = ccda.to_message()
+# My modification starts here...
 
-print ccda.to_json()
+#print ccda  # ccda Object will parse entire C-CDA XML file to produce ProtoRPC message or CSV document
+ccda_message = ccda.to_message()        # Using ccda object, we create ProtoRPC message for entire C-CDA XML
+#print ccda_message
+ccda_csv = ccda.to_csv()                # Using ccda object, we create CSV document object
+#print ccda_csv
+
+#print ccda_message.medications   # ProtoRPC message for tag 'Medication" in given C-CDA XML file from folder testdata
+print "Allergies in ProtoRPC message:"
+print ccda_message.allergies     # ProtoRPC message for tag 'Allergy" in given C-CDA XML file from folder testdata
+
+#print "Complete JSON for given C-CDA XML"
+complete_JSON = ccda.to_json()   # Apply to_json() from ccda.py to produce string from C-CDA XML
+#print complete_JSON
+#print type(complete_JSON)
+
+
+JSON_file = json.loads(complete_JSON)  # Convert complete_JSON from string to JSON file
+#print "JSON file:"
+#print JSON_file
+print "\nJSON file for Allergies:"
+print JSON_file["allergies"]   # Entries for allergies from JSON_file
+
+# Mymodification ends here ....
